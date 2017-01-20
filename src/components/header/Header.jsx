83232cd518131e547c18229e9d5b15c63dbdd350
Alfred Cao <alfred.cao@arcticwolf.com>
2017-01-19 21:11:02 -0500

    add bootstrap css, construc header component

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+  render(){
+    return (
+      <div className="page-header">
+          <h1>This is the page header</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
