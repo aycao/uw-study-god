f74ecd8ddcebc81c9ce15199a03ae2f711339dd2
aycao <alfred.yushi.cao@gmail.com>
2017-02-01 17:20:29 -0500

    finish search input and it's styling

--- src/components/side-nav/CourseCodeSearchBar.jsx
+++ src/components/side-nav/CourseCodeSearchBar.jsx
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import _ from 'lodash';
+import {Glyphicon} from 'react-bootstrap';
 import Fuse from 'fuse.js'
 
-import {setCourseCodeFilterAC} from '../../actions';
+import {setCourseCodeFilterAC, setFilteredCourseCodesAC} from '../../actions';
 
 class CourseCodeSearchBar extends Component{
   constructor(props){
@@ -11,28 +12,37 @@ class CourseCodeSearchBar extends Component{
     this.handleSearchBarInput = this.handleSearchBarInput.bind(this);
   }
 
-  componentWillReceiveProps(nextProps){
-    if(!this.fuse && !_.isEmpty(nextProps.courseCodes.all)){
-      console.log("component will receive props");
-      const options = {
-        keys: ["subject", "title"],
-      };
-      this.fuse = new Fuse(nextProps.courseCodes.all, options);
-    }
-  }
-
   render(){
     return (
         <div className="course-code-search-bar-container">
-          <input type="text"
-                 value={this.props.courseCodes.filter.subject || ''}
-                 onInput={this.handleSearchBarInput}/>
+            <Glyphicon glyph="search"/>
+            <input type="text"
+                   className="course-code-search-input"
+                   placeholder="Search"
+                   value={this.props.courseCodes.filter.subject || ''}
+                   onInput={this.handleSearchBarInput}/>
         </div>
     );
   }
 
   handleSearchBarInput(evt){
-    this.props.setFilteredCourseCodes(this.fuse.search(evt.target.value));
+    if(!this.fuse){
+      if(!this.props.courseCodes.all){
+        return;
+      }
+      const options = {
+        keys: ['subject', 'description'],
+        shouldSort: false,
+        threshold: 0.3
+      };
+      this.fuse = new Fuse(this.props.courseCodes.all,options);
+    }
+    const fuseResult = this.fuse.search(evt.target.value);
+    if(_.isEmpty(fuseResult)){
+      this.props.setFilteredCourseCodes(this.props.courseCodes.all);
+    }else{
+      this.props.setFilteredCourseCodes(fuseResult);
+    }
     this.props.setCourseFilter({subject: evt.target.value});
   }
 }
@@ -49,7 +59,7 @@ const mapDispatchToProps = (dispatch) => {
       dispatch(setCourseCodeFilterAC(filter))
     },
     setFilteredCourseCodes: (courseCodes) => {
-      dispatch(setCourseCodeFilterAC(courseCodes))
+      dispatch(setFilteredCourseCodesAC(courseCodes))
     },
   }
 };
